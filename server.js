5ff4e342747204face4c8bda7df4cf8d99acbfe8
Alan Taylor <alancarltaylor@gmail.com>
2016-07-12 19:41:53 -0600

    code polished

--- server.js
+++ server.js
@@ -11,17 +11,20 @@ app.get('/', function(req, res) {
 
 
 app.get('/wikipedia/:deathYear', function(req, res) {
+
   getPerson();
+
   function getPerson(){
     var deathYear = req.params.deathYear;
     var url = 'https://en.wikipedia.org/w/api.php?&action=query&cmtitle=category:' + deathYear + '%20deaths&list=categorymembers&cmlimit=max&format=json';
 
     return rp(url, {
       json: true
-    }).then(function(response) {
+    })
+
+    .then(function(response) {
       var arrLen = response.query.categorymembers.length;
       var randomPerson = response.query.categorymembers[Math.floor(Math.random() * (arrLen-1))];
-      console.log(randomPerson);
       var pageId = randomPerson.pageid;
       var name = randomPerson.title;
 
@@ -33,47 +36,62 @@ app.get('/wikipedia/:deathYear', function(req, res) {
         imageUrl: null,
         bio: null
       };
+    })
 
-    }).then(function(person) {
+    .then(function(person) {
       var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=categories&titles=' + person.name;
 
-      return rp(url, {json: true}).then(function(response) {
+      return rp(url, {
+        json: true
+      })
+
+      .then(function(response) {
         var birthYear = (response.query.pages[person.pageId].categories[0].title.match(/\d{1,4}/)[0]) - 1;
         var deathYear = (response.query.pages[person.pageId].categories[1].title.match(/\d{1,4}/)[0]) - 1;
-        console.log(birthYear, deathYear);
         person.birthYear = birthYear ? Number(birthYear) : 1750;
         person.deathYear = deathYear ? Number(deathYear) : 1850;
         return person;
       });
-    }).then(function(person) {
+    })
+
+    .then(function(person) {
       var url = 'https://en.wikipedia.org/w/api.php?&action=query&titles=' + person.name + '&prop=pageimages&format=json';
 
       return rp(url, {
         json: true
-      }).then(function(response) {
+      })
+
+      .then(function(response) {
         var image = response.query.pages[person.pageId].thumbnail ? response.query.pages[person.pageId].thumbnail.source.replace(/\d+px/, '129px') : null;
         person.imageUrl = image;
         return person;
       });
-    }).then(function(person) {
+    })
+
+    .then(function(person) {
       var url = 'https://en.wikipedia.org/w/api.php?&action=query&titles=' + person.name + '&prop=extracts&exintro=&format=json';
 
       return rp(url, {
         json: true
-      }).then(function(response) {
+      })
+
+      .then(function(response) {
         var bio = response.query.pages[person.pageId].extract;
         person.bio = bio;
         return person;
       });
-    }).then(function(person) {
+    })
+
+    .then(function(person) {
       res.json(person);
-    }).catch(function(e) {
-      console.log('=========================', 'FUCK YOU ERROR', '=========================');
+    })
+
+    .catch(function(e) {
+      console.log('=========================ERROR=========================');
       getPerson();
     });
   }
 });
-
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('Reincarnation App is running on localhost:3000!');
